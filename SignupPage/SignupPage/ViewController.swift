91596466a8edaf24336415ecde2def24dec6e817
Wei Yuxin <weiyuxin100@gmail.com>
2017-12-28 21:54:42 +0800

    SIGNUP PAGE

---
+++ SignupPage/SignupPage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SignupPage
+//
+//  Created by Wei Yuxin on 28/12/17.
+//  Copyright © 2017 Wei Yuxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    //chagnge
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
