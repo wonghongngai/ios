e438117353916fe5873e6c678cb21ab6ef35094c
weiyushit <wei.yuxin14@iamtemasek.edu.sg>
2017-12-28 21:20:21 +0800

    Add files via upload

---
+++ TmsRead ios about/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TmsRead ios about
+//
+//  Created by Ong Teng Wee on 26/12/17.
+//  Copyright © 2017 Ong Teng Wee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var TermsOfUse: UIButton!
+    @IBOutlet weak var PrivacyPolicy: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
