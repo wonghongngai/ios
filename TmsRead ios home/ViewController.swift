1f048c13f7c28ec40657ad28492b5938daffb041
weiyushit <wei.yuxin14@iamtemasek.edu.sg>
2017-12-28 21:30:23 +0800

    Add files via upload

---
+++ TmsRead ios home/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  TmsRead ios home
+//
+//  Created by Ong Teng Wee on 26/12/17.
+//  Copyright © 2017 Ong Teng Wee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+
+    @IBOutlet weak var rec4: UIButton!
+    @IBOutlet weak var rec3: UIButton!
+    @IBOutlet weak var rec2: UIButton!
+    @IBOutlet weak var rec1: UIButton!
+    @IBOutlet weak var popular4: UIButton!
+    @IBOutlet weak var popular3: UIButton!
+    @IBOutlet weak var popular2: UIButton!
+    @IBOutlet weak var popular1: UIButton!
+    @IBOutlet weak var newArrival4: UIButton!
+    @IBOutlet weak var newArrival3: UIButton!
+    @IBOutlet weak var newArrival2: UIButton!
+    @IBOutlet weak var newArrival1: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
