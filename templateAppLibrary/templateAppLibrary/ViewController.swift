0543c3c6f9280c3a435c818f2c561d0b8483b5e6
Julius Bryan <34909050+SystemNetworks@users.noreply.github.com>
2018-01-10 18:22:23 +0800

    Template

---
+++ templateAppLibrary/templateAppLibrary/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  templateAppLibrary
+//
+//  Created by Julie Christie Gambe on 8/1/18.
+//  Copyright © 2018 Julius Bryan Gambe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+/*
+     Note: Upper Horizontal Stack View for navigation bar are for UI ImageView for navigation icons
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
